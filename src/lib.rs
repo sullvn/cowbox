35c89aa22e480d74a64a4b23be804b0a364c912c
Kevin Sullivan <kevin@sull.vn>
2023-03-22 17:45:54 -0500

    Working MacOS interposing proof-of-concept

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,42 @@
 use std::ffi::{c_char, c_int};
 
+extern "system" {
+    fn unlink(_path: *const c_char) -> c_int;
+    fn unlinkat(_dir_fd: c_int, _path: *const c_char, _flags: c_int) -> c_int;
+}
+
 #[no_mangle]
-pub extern "system" fn unlink(_path: *const c_char) -> c_int {
+pub extern "system" fn unlink_new(_path: *const c_char) -> c_int {
     0
 }
 
 #[no_mangle]
-pub extern "system" fn unlinkat(_dir_fd: c_int, _path: *const c_char, _flags: c_int) -> c_int {
+pub extern "system" fn unlinkat_new(_dir_fd: c_int, _path: *const c_char, _flags: c_int) -> c_int {
     0
 }
+
+#[repr(C)]
+struct InterposedUnlink {
+    replacement: unsafe extern "system" fn(*const c_char) -> c_int,
+    original: unsafe extern "system" fn(*const c_char) -> c_int,
+}
+
+#[repr(C)]
+struct InterposedUnlinkAt {
+    replacement: unsafe extern "system" fn(c_int, *const c_char, c_int) -> c_int,
+    original: unsafe extern "system" fn(c_int, *const c_char, c_int) -> c_int,
+}
+
+#[used]
+#[link_section = "__DATA,__interpose"]
+static INTERPOSE_UNLINK: InterposedUnlink = InterposedUnlink {
+    replacement: unlink_new,
+    original: unlink,
+};
+
+#[used]
+#[link_section = "__DATA,__interpose"]
+static INTERPOSE_UNLINKAT: InterposedUnlinkAt = InterposedUnlinkAt {
+    replacement: unlinkat_new,
+    original: unlinkat,
+};
