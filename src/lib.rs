fdbfcdc764a275ccc72ef0f3cba7d58d8542785d
Kevin Sullivan <kevin@sull.vn>
2023-03-24 16:39:45 -0500

    Split modules for each platform

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,10 @@
-use std::ffi::{c_char, c_int};
+mod libc;
 
-extern "system" {
-    fn unlink(_path: *const c_char) -> c_int;
-    fn unlinkat(_dir_fd: c_int, _path: *const c_char, _flags: c_int) -> c_int;
-}
+#[cfg(target_os = "linux")]
+mod linux;
 
-#[no_mangle]
-pub extern "system" fn unlink_new(_path: *const c_char) -> c_int {
-    0
-}
+#[cfg(target_os = "macos")]
+mod macos;
 
-#[no_mangle]
-pub extern "system" fn unlinkat_new(_dir_fd: c_int, _path: *const c_char, _flags: c_int) -> c_int {
-    0
-}
-
-#[repr(C)]
-struct Interposed<T> {
-    replacement: T,
-    original: T,
-}
-
-#[used]
-#[link_section = "__DATA,__interpose"]
-static INTERPOSE_UNLINK: Interposed<unsafe extern "system" fn(*const c_char) -> c_int> =
-    Interposed {
-        replacement: unlink_new,
-        original: unlink,
-    };
-
-#[used]
-#[link_section = "__DATA,__interpose"]
-static INTERPOSE_UNLINKAT: Interposed<
-    unsafe extern "system" fn(c_int, *const c_char, c_int) -> c_int,
-> = Interposed {
-    replacement: unlinkat_new,
-    original: unlinkat,
-};
+#[cfg(target_os = "windows")]
+mod windows;
---
+++ src/macos.rs
@@ -0,0 +1,36 @@
+use std::ffi::{c_char, c_int};
+
+use crate::libc;
+
+#[no_mangle]
+pub extern "system" fn unlink_new(_path: *const c_char) -> c_int {
+    0
+}
+
+#[no_mangle]
+pub extern "system" fn unlinkat_new(_dir_fd: c_int, _path: *const c_char, _flags: c_int) -> c_int {
+    0
+}
+
+#[repr(C)]
+struct Interposed<T> {
+    replacement: T,
+    original: T,
+}
+
+#[used]
+#[link_section = "__DATA,__interpose"]
+static INTERPOSE_UNLINK: Interposed<unsafe extern "system" fn(*const c_char) -> c_int> =
+    Interposed {
+        replacement: unlink_new,
+        original: libc::unlink,
+    };
+
+#[used]
+#[link_section = "__DATA,__interpose"]
+static INTERPOSE_UNLINKAT: Interposed<
+    unsafe extern "system" fn(c_int, *const c_char, c_int) -> c_int,
+> = Interposed {
+    replacement: unlinkat_new,
+    original: libc::unlinkat,
+};
