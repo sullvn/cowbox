a27bb5cbc14710540df1abea1a49310a06c3f7ae
Kevin Sullivan <kevin@sull.vn>
2023-03-30 15:06:20 -0500

    Complete basic translation from C++ example

--- src/windows.rs
+++ src/windows.rs
@@ -2,6 +2,8 @@ use core::ffi::{c_ulong, c_void};
 use windows_sys::core::{PCSTR, PCWSTR};
 use windows_sys::Win32::Foundation::{BOOL, HINSTANCE, TRUE, FALSE};
 use windows_sys::Win32::System::SystemServices::{DLL_PROCESS_ATTACH, DLL_PROCESS_DETACH};
+use windows_sys::Win32::System::Threading::GetCurrentThread;
+use windows_sys::Win32::Storage::FileSystem::{DeleteFileA, DeleteFileW};
 
 use crate::detours::{
     DetourAttach,
@@ -14,12 +16,12 @@ use crate::detours::{
 };
 
 #[no_mangle]
-pub unsafe extern "system" fn DeleteFileA(_path: PCSTR) -> BOOL {
+pub unsafe extern "system" fn DeleteFileA_new(_path: PCSTR) -> BOOL {
     0
 }
 
 #[no_mangle]
-pub unsafe extern "system" fn DeleteFileW(_path: PCWSTR) -> BOOL {
+pub unsafe extern "system" fn DeleteFileW_new(_path: PCWSTR) -> BOOL {
     0
 }
 
@@ -32,21 +34,26 @@ pub unsafe extern "system" DllMain(
     reason: c_ulong,
     reserved: *mut c_void,
 ) -> BOOL {
-    if (DetourIsHelperThread) {
+    if (DetourIsHelperThread()) {
         return TRUE;
     }
+
+    let DeleteFileA_variable = DeleteFileA;
+    let DeleteFileW_variable = DeleteFileW;
 
     if (reason == DLL_PROCESS_ATTACH) {
         DetourRestoreAfterWith();
 
         DetourTransactionBegin();
         DetourUpdateThread(GetCurrentThread());
-        DetourAttach(&(PVOID&)TrueSleep, TimedSleep);
+        DetourAttach(&DeleteFileA_variable, DelteFileA_new);
+        DetourAttach(&DeleteFileW_variable, DelteFileW_new);
         DetourTransactionCommit();
     } else if (reason == DLL_PROCESS_DETACH){
         DetourTransactionBegin();
         DetourUpdateThread(GetCurrentThread());
-        DetourDetach(&(PVOID&)TrueSleep, TimedSleep);
+        DetourDetach(&DeleteFileA_variable, DelteFileA_new);
+        DetourDetach(&DeleteFileW_variable, DelteFileW_new);
         DetourTransactionCommit();
     }
 
