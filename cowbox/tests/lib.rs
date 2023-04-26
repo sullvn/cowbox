0d85a42e5249a93bdecf7689320d0a94c122e2ec
Kevin Sullivan <kevin@sull.vn>
2023-04-26 18:00:14 -0500

    Remove test debugging

--- cowbox/tests/lib.rs
+++ cowbox/tests/lib.rs
@@ -3,11 +3,8 @@ use cowbox_testing::{run_test_rm, RmResult};
 use std::io::Result;
 
 #[test]
-fn sandboxed_sip_rm() -> Result<()> {
+fn sandboxed_rm() -> Result<()> {
     let rm_result = run_test_rm(env!("CARGO_TARGET_TMPDIR"), |file_path, tmp_dir| {
-        dbg!(tmp_dir);
-        dbg!("rm");
-        dbg!(file_path);
         let exit_code = spawn(tmp_dir, "rm", [file_path])?;
         Ok(exit_code == 0)
     })?;
