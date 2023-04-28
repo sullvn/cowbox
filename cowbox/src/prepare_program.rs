4b8d852f0a4e2eb01e3174b7cba767b0fcd3f269
Kevin Sullivan <kevin@sull.vn>
2023-04-28 14:44:29 -0500

    Put macOS `prepare_program` in module

--- cowbox/src/prepare_program.rs
+++ cowbox/src/prepare_program.rs
@@ -1,9 +1,7 @@
 use std::borrow::Cow;
 use std::ffi::OsStr;
-use std::fs;
-use std::io::{Result, Error, ErrorKind};
-use std::path::{Path, PathBuf};
-use which::which;
+use std::io::Result;
+use std::path::Path;
 
 #[cfg(not(target_os = "macos"))]
 pub fn prepare_program<'a>(_injection_dir: &Path, program: &'a OsStr) -> Result<Cow<'a, OsStr>>
@@ -12,14 +10,28 @@ pub fn prepare_program<'a>(_injection_dir: &Path, program: &'a OsStr) -> Result<
 }
 
 #[cfg(target_os = "macos")]
-pub fn prepare_program<'a>(injection_dir: &Path, program: &'a OsStr) -> Result<Cow<'a, OsStr>>
-{
-    let program_path = which(program).map_err(|e| {
-        Error::new(ErrorKind::Other, e)
-    })?;
+pub use macos::prepare_program;
 
-    let copy_path: PathBuf = [injection_dir.as_ref(), program].iter().collect();
-    fs::copy(program_path, &copy_path)?;
+mod macos {
+    #![cfg(target_os = "macos")]
 
-    Ok(Cow::Owned(copy_path.into_os_string()))
+    use std::fs;
+    use std::io::{Error, ErrorKind};
+    use std::path::PathBuf;
+    use which::which;
+
+    use super::{Cow, OsStr, Path, Result};
+
+    pub fn prepare_program<'a>(injection_dir: &Path, program: &'a OsStr) -> Result<Cow<'a, OsStr>>
+    {
+        let program_path = which(program).map_err(|e| {
+            Error::new(ErrorKind::Other, e)
+        })?;
+
+        let copy_path: PathBuf = [injection_dir.as_ref(), program].iter().collect();
+        fs::copy(program_path, &copy_path)?;
+
+        Ok(Cow::Owned(copy_path.into_os_string()))
+    }
 }
+
