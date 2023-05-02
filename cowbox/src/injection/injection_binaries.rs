b583c0d8d4b43f2d7e22ffbe7f0221653d69e5f2
Kevin Sullivan <kevin@sull.vn>
2023-05-02 13:51:31 -0500

    Injection binary documentation

--- cowbox/src/injection/injection_binaries.rs
+++ cowbox/src/injection/injection_binaries.rs
@@ -3,15 +3,30 @@ use std::path::Path;
 
 use super::injection_binary::InjectionBinary;
 
+/// Multiple `InjectionBinary` definitions
+/// for a single platform
 pub struct InjectionBinaries {
     injection_binaries: &'static [InjectionBinary],
 }
 
 impl InjectionBinaries {
+    ///
+    /// Define all injection binaries for
+    /// the platform
+    ///
+    /// NOTE: First binary will be the
+    /// "preferred" one. See [`Self::preferred()`]
+    /// for more details.
+    ///
     pub const fn new(injection_binaries: &'static [InjectionBinary]) -> Self {
         Self { injection_binaries }
     }
 
+    /// Update all defined binaries.
+    ///
+    /// TODO: Investigate sharing work
+    /// between individual binary updates.
+    ///
     pub fn update<P: AsRef<Path>>(&self, dir: P) -> Result<()> {
         for ib in self.injection_binaries {
             ib.update(&dir)?;
@@ -20,6 +35,15 @@ impl InjectionBinaries {
         Ok(())
     }
 
+    /// Preferred injection binary to provide
+    /// the OS, if there are multiple.
+    ///
+    /// At the moment, this is used on Windows
+    /// in order to properly handle injection
+    /// into both x86 and x86_64 programs. The
+    /// provided DLL has to the same
+    /// architecture as the `cowbox` program.
+    ///
     pub fn preferred(&self) -> &'static InjectionBinary {
         self.injection_binaries.get(0).expect("at least one injection binary should be defined")
     }
--- cowbox/src/injection/injection_binary.rs
+++ cowbox/src/injection/injection_binary.rs
@@ -3,9 +3,34 @@ use std::fs::{self, create_dir_all, read_to_string};
 use std::io::Result;
 use std::path::{Path, PathBuf};
 
+/// Injection binary definition
+///
+/// The binary, expected to be a dynamic
+/// linked library, is loaded into the
+/// target program on creation. Standard
+/// OS calls in the program are detoured
+/// through cowbox's middleware. 
+///
 pub struct InjectionBinary {
+    ///
+    /// Payload of binary
+    ///
+    /// Statically linked into `cowbox`
+    /// for easy deployment. The
+    /// payload, a dynamic linked
+    /// library, is written to file as
+    /// required.
+    ///
     bytes: &'static [u8],
+
+    /// Filename of binary when it is
+    /// deployed onto the filesystem
     file_name: &'static str,
+
+    /// Hash of binary used for
+    /// invalidation of any injection
+    /// binary already deployed onto
+    /// the file system
     hash: u128,
 }
 
@@ -14,10 +39,21 @@ impl InjectionBinary {
         Self {
             bytes,
             file_name,
+            // TODO: Actually compute hash
             hash: 0,
         }
     }
 
+    /// Update binary on filesystem
+    ///
+    /// Should be done on every instantiation
+    /// of `cowbox` so the latest payload is
+    /// used.
+    ///
+    /// WARNING: This process is probably the
+    /// slowest part of `cowbox`. Avoid as
+    /// many I/O calls as possible.
+    ///
     pub fn update<P: AsRef<Path>>(&self, dir: P) -> Result<()> {
         if let Some(true) = self.exists(&dir) {
             return Ok(());
@@ -26,10 +62,24 @@ impl InjectionBinary {
         self.create(&dir)
     }
 
+    /// Binary payload path
+    ///
+    /// The standard OS-specific file name
+    /// is used, placed within the given
+    /// directory.
+    ///
     pub fn binary_path<P: AsRef<Path>>(&self, dir: P) -> PathBuf {
         [dir.as_ref(), self.file_name.as_ref()].iter().collect()
     }
 
+    /// Check if everything exists correctly
+    /// on the file system.
+    ///
+    /// The expectation is that this will be
+    /// true 99% of the time, besides on
+    /// the first run and any subsequent
+    /// updates of `cowbox`.
+    ///
     fn exists<P: AsRef<Path>>(&self, dir: P) -> Option<bool> {
         self.binary_path(&dir).try_exists().ok()?;
 
@@ -39,6 +89,25 @@ impl InjectionBinary {
         Some(hash_matches)
     }
 
+    /// Create injection binary and metadata
+    /// (eg. hash) on the filesystem
+    ///
+    /// QUESTION: Is there a way to make this
+    /// update process atomic. Does it even
+    /// matter that much?
+    ///
+    /// Example: Binary could have the hash
+    /// as its file name. A symlink, with the
+    /// standard "nice" dylib name, could
+    /// link to the hash. Updating the symlink
+    /// location is an atomic operation. 
+    ///
+    /// Problem: For some reason, symlinks
+    /// [require administrator privileges on
+    /// Windows][0].
+    ///
+    /// [0]: https://learn.microsoft.com/en-us/windows/security/threat-protection/security-policy-settings/create-symbolic-links#default-values
+    ///
     fn create<P: AsRef<Path>>(&self, dir: P) -> Result<()> {
         let hash_str = format!("{:x}", self.hash);
 
@@ -49,6 +118,11 @@ impl InjectionBinary {
         Ok(())
     }
 
+    /// Hash file path
+    ///
+    /// NOTE: See `create` documentation for
+    /// this could evolve.
+    ///
     fn hash_path<P: AsRef<Path>>(&self, dir: P) -> PathBuf {
         let mut bp = self.binary_path(&dir);
         let extension = bp.extension().unwrap_or(OsStr::new(""));
