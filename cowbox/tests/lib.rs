f5898e76ab8b733a7519a53a092d5d62707fdfe9
Kevin Sullivan <kevin@sull.vn>
2023-05-01 20:00:34 -0500

    Configure `unix` instead of `not(windows)`

--- cowbox/tests/lib.rs
+++ cowbox/tests/lib.rs
@@ -3,7 +3,7 @@ use cowbox_testing::{run_test_rm, RmResult};
 use std::io::Result;
 
 #[test]
-#[cfg(not(windows))]
+#[cfg(unix)]
 fn sandboxed_rm() -> Result<()> {
     let rm_result = run_test_rm(env!("CARGO_TARGET_TMPDIR"), |file_path, tmp_dir| {
         let exit_code = spawn(tmp_dir, "rm", [file_path])?;
