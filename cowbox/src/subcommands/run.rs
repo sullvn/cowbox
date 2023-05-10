2054e7cfbdc655e3272d087e494fdfeda1143ded
Kevin Sullivan <kevin@sull.vn>
2023-05-10 16:54:02 -0500

    Working basic CLI tool

--- cowbox/src/subcommands/run.rs
+++ cowbox/src/subcommands/run.rs
@@ -1,10 +1,16 @@
 use cowbox::spawn;
-use std::io::Result;
+use std::io::{Error, ErrorKind, Result};
 use std::process::ExitCode;
 
 use crate::args;
 use crate::cache_dir::cache_dir_path;
 
 pub fn run(args: args::Run) -> Result<ExitCode> {
-    spawn(cache_dir_path()?, args.program, args.program_args)
+    let exit_code_i32 = spawn(cache_dir_path()?, args.program, args.program_args)?;
+    let exit_code_u8: u8 = exit_code_i32
+        .try_into()
+        .map_err(|err| Error::new(ErrorKind::Other, err))?;
+    let exit_code = ExitCode::from(exit_code_u8);
+
+    Ok(exit_code)
 }
