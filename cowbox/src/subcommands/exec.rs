0a4f69a9e5e24ad2d2773668ade65919af6603ed
Kevin Sullivan <kevin@sull.vn>
2023-05-11 17:03:55 -0500

    Default to sane error exit code on Windows

--- cowbox/src/subcommands/exec.rs
+++ cowbox/src/subcommands/exec.rs
@@ -1,5 +1,5 @@
 use cowbox::spawn;
-use std::io::{Error, ErrorKind, Result};
+use std::io::Result;
 use std::process::ExitCode;
 
 use crate::args::ExecArgs;
@@ -17,24 +17,27 @@ pub fn exec(args: ExecArgs) -> Result<ExitCode> {
     let exit_code_i32 = spawn(cache_dir_path()?, args.program, args.program_args)?;
 
     // Smush spawned process return code
-    // into [`ExitCode`][0].
+    // into smaller, standard [`ExitCode`][0].
     //
     // [`ExitCode::from_raw`][1] would be
     // useful here to avoid smushing on
     // Windows. Alas, it hasn't landed
     // on stable Rust yet.
     //
-    // In any case, the returned value
-    // from [`cowbox::spawn`] would have
-    // to be a platform-agnostic wrapper.
+    // For now, default to [`ExitCode::FAILURE`][2]
+    // if the spawned process exit code is too big,
+    // as it can be on Windows.
+    //
+    // TODO: Move to using `ExitCode::from_raw`
+    // when available.
     //
     // [0]: `std::process::ExitCode`
     // [1]: `std::process::ExitCode::from_raw`
+    // [2]: `std::process::ExitCode::FAILURE`
     //
-    let exit_code_u8: u8 = exit_code_i32
-        .try_into()
-        .map_err(|err| Error::new(ErrorKind::Other, err))?;
-    let exit_code = ExitCode::from(exit_code_u8);
+    let exit_code = TryInto::<u8>::try_into(exit_code_i32)
+        .map(ExitCode::from)
+        .unwrap_or(ExitCode::FAILURE);
 
     Ok(exit_code)
 }
